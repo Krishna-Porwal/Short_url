323fa465ced8ab6a469bd3033a95cfeb97754e2f
Krishna-Porwal <khabart590@gmail.com>
2025-11-17 15:34:26 +0530

    21

--- controllers/url.js
+++ controllers/url.js
@@ -1,11 +1,11 @@
-const {shortid} = require('shortid');
+const {nanoid} = require('nanoid');
 const URL = require('../models/url');
 async function handleGenerateShortUrl(req,res){
     const body = req.body;
     if(!body.url){
         return res.status(400).json({error: 'URL is required'});
     }
-    const shortId = shortid();
+    const shortId = nanoid(8);
     await URL.create({
         shortId: shortId,
         redirectUrl: body.url,
@@ -13,4 +13,13 @@ async function handleGenerateShortUrl(req,res){
     });
     return res.json({id: shortId});
 }
-module.exports = {handleGenerateShortUrl};
+
+async function handleGetAnalytics(req,res){
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({shortId});
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        visitHistory: result.visitHistory,
+    });
+}
+module.exports = {handleGenerateShortUrl,handleGetAnalytics};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {connectToMongoDB} = require('./connect');
 const urlRoutes = require('./routes/url');
+const URL = require('./models/url');
 const app = express();
 const port = 8001;
 
@@ -9,6 +10,29 @@ connectToMongoDB('mongodb://localhost:27017/Short-url')
 .catch((err) => console.error('Failed to connect to MongoDB', err));
 
 app.use(express.json());
-app.use('/',urlRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use('/url',urlRoutes);
+
+app.get('/:shortId',async(req,res)=>{
+    const shortId = req.params.shortId;
+   const entry = await URL.findOneAndUpdate(
+    {
+        shortId
+    },{ 
+            $push: {
+                visitHistory: {
+                    timestamp: Date.now(),
+                },
+            }
+        }
+   
+    );
+    res.redirect(entry.redirectUrl);
+
+    });
+
+ 
+
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
