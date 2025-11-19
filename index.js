467b95974f5ce15c2d520ec26de6cf252557b9a8
Krishna-Porwal <khabart590@gmail.com>
2025-11-19 13:27:29 +0530

    ejs ongoing

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
 const express = require('express');
-const {connectToMongoDB} = require('./connect');
+const { connectToMongoDB } = require('./connect');
 const urlRoutes = require('./routes/url');
 const URL = require('./models/url');
+const path = require('path');
 const app = express();
 const port = 8001;
 
 connectToMongoDB('mongodb://localhost:27017/Short-url')
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Failed to connect to MongoDB', err));
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Failed to connect to MongoDB', err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/url',urlRoutes);
 
-app.get('/:shortId',async(req,res)=>{
+
+app.set('view engine', 'ejs');
+app.set('views',path.resolve("./views"));
+app.use('/url', urlRoutes);
+
+
+
+app.get('/short/:shortId', async (req, res) => {
     const shortId = req.params.shortId;
-   const entry = await URL.findOneAndUpdate(
-    {
-        shortId
-    },{ 
-            $push: {
-                visitHistory: {
-                    timestamp: Date.now(),
-                },
-            }
+    const entry = await URL.findOneAndUpdate(
+        {
+            shortId
+        }, {
+        $push: {
+            visitHistory: {
+                timestamp: Date.now(),
+            },
         }
-   
+    }
+
     );
-    res.redirect(entry.redirectUrl);
+    res.redirect(entry?.redirectUrl);
 
-    });
+});
 
- 
+app.get('/test', async (req, res) => {
+    const allUrls = await URL.find({});
+    return res.render('home');
+});
+
+
+
+
+
+
+
+
 
 
 
